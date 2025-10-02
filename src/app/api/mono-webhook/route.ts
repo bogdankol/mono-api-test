d881ca39705d61e621092d764ff907f361f6ba64
bogdankol <68349689+bogdankol@users.noreply.github.com>
2025-10-02 15:59:03 +0300

    chanched webhook to console.log only body

--- src/app/api/mono-webhook/route.ts
+++ src/app/api/mono-webhook/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST(req: NextRequest) {
   const body = await req.json()
 
-  console.log('I am webhook', { req, body })
+  console.log('I am webhook', { body })
 }
 
 export async function GET() {
