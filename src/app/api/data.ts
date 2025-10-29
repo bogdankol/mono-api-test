522cd9b09081e99437f3ba61c0af9fdea8fcfaf8
bogdankol <68349689+bogdankol@users.noreply.github.com>
2025-10-29 20:21:49 +0200

    format change

--- src/app/api/data.ts
+++ src/app/api/data.ts
@@ -1,2 +1,2 @@
-export let numbers = [1, 2, 3];
-export let clients: { resolve: (value: any) => void; lastId: number }[] = [];
+export let numbers = [1, 2, 3]
+export let clients: { resolve: (value: any) => void; lastId: number }[] = []
