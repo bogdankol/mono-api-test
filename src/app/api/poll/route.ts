aa00e0ff67ce00c7f5035eefa1aa898d717914af
bogdankol <68349689+bogdankol@users.noreply.github.com>
2025-10-29 20:08:29 +0200

    test

--- src/app/api/poll/route.ts
+++ src/app/api/poll/route.ts
@@ -43,5 +43,5 @@ export async function POST(req: NextRequest) {
   waitingClients = [];
   notifications = [];
   
-  return NextResponse.json({ success: true });
+  return NextResponse.json({ success: true })
 }
